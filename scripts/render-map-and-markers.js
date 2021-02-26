bf8643e8810c5af9402d60278b3c9b123795255b
Jigar Patel <jigar.moyo@gmail.com>
2021-02-26 04:02:49 +0000

    add info windows to maps page

--- scripts/render-map-and-markers.js
+++ scripts/render-map-and-markers.js
@@ -1,16 +1,26 @@
 let map;
 
-const initialMapZoomCoordinates = { lat: 41.789722, lng: -87.599724 }
+const initialMapViewCoordinates = { lat: 41.789722, lng: -87.599724 }
 
-function createMaker(coordinates){
-  new google.maps.Marker({
+function createMaker(coordinates, infoWindowContent, map){
+  
+  const marker = new google.maps.Marker({
     position: coordinates,
     map,
     title: "Chicago Owned Land!",
   }); 
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: infoWindowContent,
+  });
+
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+
 }
 
-function renderLibraryData(apiURL){
+function renderLibraryData(apiURL, map){
     
     var requestOptions = {
         method: 'GET',
@@ -24,6 +34,18 @@ function renderLibraryData(apiURL){
         let i;
         for (i = 0; i < result.length ; i++) {
             const currentProperty = result[i]
+            const community_area_name = currentProperty.community_area_name
+            const address = currentProperty.address;
+            const squareFootage = currentProperty.sq_ft;
+
+            const infoWindowContent = `<p>community_area_name: ${community_area_name}</p>
+                                       <p>address: ${address}</p>
+                                       <p>squareFootage: ${squareFootage}</p>`
+
+            console.info(infoWindowContent)
+
+
+
 
             if(currentProperty.location.latitude == undefined || currentProperty.location.longitude == undefined)
                 continue            
@@ -33,7 +55,7 @@ function renderLibraryData(apiURL){
             const longitude = parseFloat(currentProperty.location.longitude)   
 
             const locationCoordinates = { lat: latitude, lng: longitude }
-            createMaker(locationCoordinates)
+            createMaker(locationCoordinates, infoWindowContent, map)
         }
 
     
@@ -47,13 +69,13 @@ function initMap() {
 
   //create initial map
   map = new google.maps.Map(document.getElementById("map"), {
-    center: initialMapZoomCoordinates,
+    center: initialMapViewCoordinates,
     zoom: 11,
   });
 
   var apiURL = "https://data.cityofchicago.org/resource/aksk-kvfp.json";
 
-  renderLibraryData(apiURL);
+  renderLibraryData(apiURL, map);
   
   
 
